9c89bde89dc83eaaa10adcd7ca863d741c2fa1c9
jordan.aitken <jordan.aitken@hmcts.net>
2026-07-14 17:05:10 +0100

    SSCSCI-1380 | fix deserialisation issue

--- src/main/java/uk/gov/hmcts/reform/sscs/models/deserialize/gaps2/MajorStatus.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/models/deserialize/gaps2/MajorStatus.java
@@ -1,20 +1,27 @@
 package uk.gov.hmcts.reform.sscs.models.deserialize.gaps2;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import java.time.ZonedDateTime;
 import lombok.Value;
 
 @Value
 public class MajorStatus implements Comparable<MajorStatus> {
-    private String bfDate;
-    private String statusId;
-    private String dateClosed;
-    private ZonedDateTime dateSet;
+    @JsonAlias("BF_Date")
+    String bfDate;
 
-    public MajorStatus(@JsonProperty("BF_Date") String bfDate,
-                       @JsonProperty("Status_Id") String statusId,
-                       @JsonProperty("Date_Closed") String dateClosed,
-                       @JsonProperty("Date_Set") ZonedDateTime dateSet) {
+    @JsonAlias("Status_Id")
+    String statusId;
+
+    @JsonAlias("Date_Closed")
+    String dateClosed;
+
+    @JsonAlias("Date_Set")
+    ZonedDateTime dateSet;
+
+    public MajorStatus( String bfDate,
+                        String statusId,
+                        String dateClosed,
+                        ZonedDateTime dateSet) {
         this.bfDate = bfDate;
         this.statusId = statusId;
         this.dateClosed = dateClosed;
--- src/main/java/uk/gov/hmcts/reform/sscs/models/deserialize/gaps2/PostponementRequests.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/models/deserialize/gaps2/PostponementRequests.java
@@ -1,19 +1,26 @@
 package uk.gov.hmcts.reform.sscs.models.deserialize.gaps2;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Value;
 
 @Value
 public class PostponementRequests {
-    private String postponementGranted;
-    private String appealHearingId;
-    private String postponementReasonId;
-    private String roleRequestedById;
+    @JsonAlias("Postponement_Granted")
+    String postponementGranted;
 
-    public PostponementRequests(@JsonProperty("Postponement_Granted") String postponementGranted,
-                       @JsonProperty("Appeal_Hearing_Id") String appealHearingId,
-                       @JsonProperty("Postponement_Reason_Id") String postponementReasonId,
-                       @JsonProperty("Role_Requested_By_Id") String roleRequestedById) {
+    @JsonAlias("Appeal_Hearing_Id")
+    String appealHearingId;
+
+    @JsonAlias("Postponement_Reason_Id")
+    String postponementReasonId;
+
+    @JsonAlias("Role_Requested_By_Id")
+    String roleRequestedById;
+
+    public PostponementRequests( String postponementGranted,
+                        String appealHearingId,
+                        String postponementReasonId,
+                        String roleRequestedById) {
         this.postponementGranted = postponementGranted;
         this.appealHearingId = appealHearingId;
         this.postponementReasonId = postponementReasonId;
